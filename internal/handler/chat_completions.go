f533d1039ebfcccbf54b4bae2828a92a065d89f8
c25423 <c25423@gmail.com>
2025-09-29 14:42:34 +0800

    fix(chat completions): Preserve incoming headers

--- internal/handler/chat_completions.go
+++ internal/handler/chat_completions.go
@@ -39,8 +39,8 @@ func NewChatCompletionsHandler() gin.HandlerFunc {
 	}
 }
 
-func transformRequest(incomingHeader http.Header, incomingBody io.Reader) (*http.Request, error) {
-	// Read and unmarshall body
+func transformRequest(incomingHeaders http.Header, incomingBody io.Reader) (*http.Request, error) {
+	// Read and unmarshal body
 	incomingBodyBytes, err := io.ReadAll(incomingBody)
 	if err != nil {
 		return nil, err
@@ -60,23 +60,35 @@ func transformRequest(incomingHeader http.Header, incomingBody io.Reader) (*http
 		return nil, err
 	}
 
-	// Build the upstream request
+	// Build URL
 	upstreamUrl := string(oaiProviderConfig.BaseUrl) + "/chat/completions"
+	// Overwite body <- extra body in config
 	for k, v := range oaiModelConfig.ExtraBody {
 		bodyMap[k] = v
 	}
+	// Overwrite body <- "model"
 	bodyMap["model"] = oaiModelConfig.Identifier
+	// Marshal body
 	upstreamBody, err := json.Marshal(bodyMap)
 	if err != nil {
 		return nil, err
 	}
+	// Build upstream request
 	req, err := http.NewRequest(http.MethodPost, upstreamUrl, bytes.NewReader(upstreamBody))
 	if err != nil {
 		return nil, err
 	}
+	// Add orginal headers
+	for k, vv := range incomingHeaders {
+		for _, v := range vv {
+			req.Header.Set(k, v)
+		}
+	}
+	// Overwrite headers <- extra headers in config
 	for k, v := range oaiModelConfig.ExtraHeaders {
 		req.Header.Set(k, v)
 	}
+	// Overwrite headers <- "Accept", "Authorization", and "Content-Type"
 	req.Header.Set("Accept", "*/*")
 	req.Header.Set("Authorization", "Bearer "+string(oaiProviderConfig.ApiKey))
 	req.Header.Set("Content-Type", "application/json")
